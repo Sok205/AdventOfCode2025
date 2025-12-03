56aea89b967b003657b61f204988e2322766c4b0
Filip Sokołowski <123462364+Sok205@users.noreply.github.com>
2025-12-03 07:55:34 +0100

    feat: initial project structure

---
+++ src/bin/day01.rs
@@ -0,0 +1,20 @@
+use advent_of_code::read_lines;
+use std::io;
+
+fn solve_part1(lines: &[String]) -> i32 {
+    0
+}
+
+fn solve_part2(lines: &[String]) -> i32 {
+    0
+}
+
+fn main() -> io::Result<()> {
+    let lines = read_lines("input/day01.txt")?;
+
+    println!("Day 1");
+    println!("Part 1: {}", solve_part1(&lines));
+    println!("Part 2: {}", solve_part2(&lines));
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    reader.lines().collect()
+}
+
+pub fn read_input<P>(filename: P) -> io::Result<String>
+where
+    P: AsRef<Path>,
+{
+    std::fs::read_to_string(filename)
+}

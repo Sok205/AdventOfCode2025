c132c2171f870cc384c5f2927121f96b8b60fac3
Filip Sokołowski <123462364+Sok205@users.noreply.github.com>
2025-12-08 16:47:48 +0100

    feat(day01): solution for part 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,7 +2,29 @@ use advent_of_code::read_lines;
 use std::io;
 
 fn solve_part1(lines: &[String]) -> i32 {
-    0
+    let mut password = 0;
+    let mut number: i32 = 50;
+
+    for line in lines {
+        let dir = line.chars().next().unwrap();
+        let value: i32 = line[1..].parse().unwrap();
+
+        match dir {
+            'R' => {
+                number = (number + value) % 100;
+            }
+            'L' => {
+                number = (number - value).rem_euclid(100);
+            }
+            _ => panic!("bomboclat"),
+        }
+
+        if number == 0 {
+            password += 1;
+        }
+    }
+
+    password
 }
 
 fn solve_part2(lines: &[String]) -> i32 {
